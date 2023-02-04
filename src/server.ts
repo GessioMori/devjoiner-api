81e424132320fa46f02d91ba8b1898c41b481b70
Gessio Mori <gessiomori@protonmail.com>
2023-02-04 11:34:05 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify';
+
+const server = fastify();
+
+server.get('/', async () => {
+	return { hello: 'world' };
+});
+
+const start = async () => {
+	try {
+		await server.listen({ port: 3000 });
+	} catch (err) {
+		server.log.error(err);
+		process.exit(1);
+	}
+};
+start();
